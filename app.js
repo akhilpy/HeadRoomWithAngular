a0aeb0f777dc0953f26a2ba0b7229430ecf8d4b5
akhilpy <akhilendra44@gmail.com>
2016-11-09 20:16:54 +0530

    Staging on server

--- app.js
+++ app.js
@@ -10,7 +10,6 @@
 				templateUrl : 'pages/home.html',
 				controller  : 'mainController'
 			})
-
 			// route for the about page
 			.when('/about', {
 				templateUrl : 'pages/about.html',
@@ -27,13 +26,15 @@
 	// create the controller and inject Angular's $scope
 	myApp.controller('mainController', function($scope) {
 		// create a message to display in our view
-		$scope.message = 'Everyone come and see how good I look!';
+
+		$scope.message = 'HI this is home page!';
 	});
 
 	myApp.controller('aboutController', function($scope) {
-		$scope.message = 'Look! I am an about page.';
+		$scope.message = 'HI, You are on about page.';
 	});
 
 	myApp.controller('contactController', function($scope) {
-		$scope.message = 'Contact us! JK. This is just a demo.';
+		$scope.message = 'Contact us Page';
+		$scope.message = 'Everyone come and see how good I look!';
 	});
